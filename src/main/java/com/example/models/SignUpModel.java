7ac927adf0ca0aa62fe35ca90fba19552685bf9d
Bossit1998 <Bossit98@mail.ru>
2020-08-19 12:18:05 +0500

    experiment

---
+++ src/main/java/com/example/models/SignUpModel.java
@@ -0,0 +1,20 @@
+package com.example.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignUpModel extends SignInModel{
+    private String name;
+    private String surName;
+    private String email;
+    private String phoneNumber;
+    private String region;
+    private String companyName;
+    private String privilege;
+}
--- src/main/java/com/example/rest/UserController.java
+++ src/main/java/com/example/rest/UserController.java
@@ -1,17 +1,21 @@
-package com.example.rest;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("api")
-public class UserController {
-
-    @GetMapping("products")
-    public String migrate_bank_machines() {
-        return "xaxa";
-    }
-
-
-}
+//package com.example.rest;
+//
+//import com.example.models.ResponseData;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("api")
+//public class UserController {
+//
+//    @GetMapping("products")
+//    public String migrate_bank_machines() {
+//        return "xaxa";
+//    }
+//
+//    @PostMapping("/delete/month")
+//    public ResponseData deleteFromStartMonth(@RequestBody String baseModel) {
+//        return service. delete_and_rewrite_service(baseModel, baseModel.getUserId());
+//    }
+//
+//
+//}
