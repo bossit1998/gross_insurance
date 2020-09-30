9e67a78d15936f2559958a1206ee79ad4f1d81f5
Bossit1998 <Bossit98@mail.ru>
2020-09-30 16:09:44 +0500

    some functions

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -18,12 +18,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 
-@Controller
 @SpringBootApplication
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 }
